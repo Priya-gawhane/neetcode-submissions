c05e776fe3ecd48ef457d2a92844a3639f2351ca
Priya-gawhane <108482615+Priya-gawhane@users.noreply.github.com>
2026-09-13 23:40:38 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = {}
+
+        for word in strs:
+            count = [0] * 26
+
+            for ch in word:
+                index = ord(ch) - ord('a')
+                count[index] += 1
+
+            key = tuple(count)
+
+            if key not in seen:
+                seen[key] = []
+
+            seen[key].append(word)
+
+        return list(seen.values())
